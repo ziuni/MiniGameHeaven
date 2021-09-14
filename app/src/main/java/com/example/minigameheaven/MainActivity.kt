74cfd7742dbdea75e61418cc2c9a413a847209e4
kimdahuin12 <s2001@e-mirim.hs.kr>
2021-09-14 22:10:39 +0900

    이미지버튼 테스트~

--- app/src/main/java/com/example/minigameheaven/MainActivity.kt
+++ app/src/main/java/com/example/minigameheaven/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.minigameheaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bell_button = findViewById<ImageButton>(R.id.bell_button)
+
+        bell_button.setOnClickListener {
+            Toast.makeText(this, "눌림", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
