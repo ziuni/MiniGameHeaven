f9037732455fc4fbfd1d015255c5cab16292dc6d
kimdahuin12 <s2001@e-mirim.hs.kr>
2021-09-23 20:04:01 +0900

    ..

--- app/src/main/java/com/example/minigameheaven/MainActivity.kt
+++ app/src/main/java/com/example/minigameheaven/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.minigameheaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageButton
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
+
+        var screenWidth = getWindowManager().getDefaultDisplay()
 
         var bell_button = findViewById<ImageButton>(R.id.bell_button)
 
